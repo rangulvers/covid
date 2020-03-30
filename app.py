e930b73095d55bd2cc5896b15edace4768f80643
m3m4s0 <m.sohns@sap.com>
2020-03-30 12:48:38 +0200

    changes

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ fig_death = px.line(x=df_death_flat["Date"], y=df_death_flat["count"],
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 app.layout = html.Div(children=[
 
     dcc.Graph(
