57980a9d335d41156be314a7c9e6fa10c7438bf7
m3m4s0 <m.sohns@sap.com>
2020-03-30 17:07:52 +0200

    added heatmap

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Dashboard to show the numbers on the current COVID-19 outbreak.
 All information is pulled from the Johns Hopkins University [Github Page](https://www.github.com/CSSEGISandData)
 
 There are many more very good Dashboards out there. This is just for me to test some code
-
+s
 # Open Topics
 * Filter by Country for all Charts with dropdown
 * Inculde sir epidemic model
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import plotly.express as px
+import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -16,14 +17,19 @@ df_death = pd.read_csv(urL_death, error_bad_lines=False)
 df_recovered = pd.read_csv(url_recover, error_bad_lines=False)
 
 # Flatten Datasets to fit charts
-df_confirmed_flat = df_confirmed.melt(var_name="Date", value_name="count", id_vars=["Province/State", "Country/Region", "Lat", "Long"])
-df_death_flat = df_death.melt(var_name="Date", value_name="count", id_vars=["Province/State", "Country/Region", "Lat", "Long"])
-df_recoverd_flat = df_recovered.melt(var_name="Date", value_name="count", id_vars=["Province/State", "Country/Region", "Lat", "Long"])
+df_confirmed_flat = df_confirmed.melt(var_name="Date", value_name="count", id_vars=[
+                                      "Province/State", "Country/Region", "Lat", "Long"])
+df_death_flat = df_death.melt(var_name="Date", value_name="count", id_vars=[
+                              "Province/State", "Country/Region", "Lat", "Long"])
+df_recoverd_flat = df_recovered.melt(var_name="Date", value_name="count", id_vars=[
+                                     "Province/State", "Country/Region", "Lat", "Long"])
 
 # Fix -1 and 0 values
-df_confirmed_flat = df_confirmed_flat[(df_confirmed_flat[['count']] > 0).all(axis=1)]
+df_confirmed_flat = df_confirmed_flat[(
+    df_confirmed_flat[['count']] > 0).all(axis=1)]
 df_death_flat = df_death_flat[(df_death_flat[['count']] > 0).all(axis=1)]
-df_recoverd_flat = df_recoverd_flat[(df_recoverd_flat[['count']] > 0).all(axis=1)]
+df_recoverd_flat = df_recoverd_flat[(
+    df_recoverd_flat[['count']] > 0).all(axis=1)]
 
 # Change Dateformat of Date Column
 df_confirmed_flat["Date"] = pd.to_datetime(df_confirmed_flat["Date"])
@@ -32,13 +38,17 @@ df_recoverd_flat["Date"] = pd.to_datetime(df_recoverd_flat["Date"])
 df_death_flat = df_death_flat.drop(columns=["Province/State", "Lat", "Long"])
 
 # Confirmed Cases prep step
-byDateCountry = df_confirmed_flat.groupby(["Date", "Country/Region"], as_index=False).sum()
+byDateCountry = df_confirmed_flat.groupby(
+    ["Date", "Country/Region"], as_index=False).sum()
 byDateCountry = byDateCountry.drop(columns=["Lat", "Long"])
 byDateWorldWide = byDateCountry.groupby("Date", as_index=False).sum()
 byDateWorldWide["pct_change"] = byDateWorldWide['count'].pct_change()
 byDateWorldWide["diff"] = byDateWorldWide['count'].diff()
 byDateWorldWide["diff_10"] = byDateWorldWide['count'].diff(periods=10)
-df_death_flat = df_death_flat.groupby(["Date", "Country/Region"], as_index=False).sum()
+df_death_flat = df_death_flat.groupby(
+    ["Date", "Country/Region"], as_index=False).sum()
+
+
 
 # Setup Charts
 fig_byDateWorldWide = px.scatter()
@@ -65,6 +75,12 @@ fig_death = px.line(x=df_death_flat["Date"],
                     color=df_death_flat["Country/Region"],
                     title="Death")
 
+fig_confirmed_heatmap = go.Figure(data=go.Heatmap(
+    z=byDateCountry["count"],
+    x=byDateCountry["Country/Region"],
+    y=byDateCountry["Date"],
+    colorscale='Viridis'))
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
@@ -73,19 +89,23 @@ app.layout = html.Div(children=[
         # COVID-19 Dashboard
         All information is pulled from the Johns Hopkins University Github Page [links](https://www.github.com/CSSEGISandData)'''),
     dcc.Graph(
-        id='con-graph-wordlwide',
+        id='fig_byDateWorldWide',
         figure=fig_byDateWorldWide
     ),
     dcc.Graph(
-        id='con-graph-wordlwide-pct',
+        id='fig_confirmed_heatmap',
+        figure=fig_confirmed_heatmap
+    ),
+    dcc.Graph(
+        id='fig_byDateWorldWidePct',
         figure=fig_byDateWorldWidePct
     ),
     dcc.Graph(
-        id='con-graph-by-date',
+        id='fig_byDateCountry',
         figure=fig_byDateCountry
     ),
     dcc.Graph(
-        id='con-map',
+        id='fig_deat',
         figure=fig_death
     )
 ])
